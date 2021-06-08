7c65077a2bd0d4767d2a92f4605820dfd7fdc366
Karthik Eetur <keetur@tcs.woolworths.com.au>
2021-06-08 21:27:19 +1000

    added monthly premium calculator logic

---
+++ PremiumCalculator/Models/Premium.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PremiumCalculator.Models
+{
+    public class Premium
+    {
+        public string Name { get; set; }
+        public decimal Age { get; set; }
+        public DateTime DOB { get; set; }
+        public string Occupation { get; set; }
+        public string SumInsured { get; set; }
+    }
+}
---
+++ PremiumCalculator/Scripts/Calculate.js
@@ -0,0 +1,48 @@
+﻿
+
+angular.module('calculateApp', []).controller('HomeController', function ($scope) {
+
+    $scope.occupationList = [
+        {
+            occupation: "Cleaner", rating: "Light Manual"
+        },
+        {
+            occupation: "Doctor", rating: "Professional"
+        },
+        {
+            occupation: "Author", rating: "White Collar"
+        },
+        {
+            occupation: "Farmer", rating: "Heavy Manual"
+        },
+        {
+            occupation: "Mechanic", rating: "Heavy Manual"
+        },
+        {
+            occupation: "Florist", "rating": "Light Manual"
+        }
+    ];
+
+    $scope.findRatingFactor = function () {
+        var ratingFactor = { "Professional": 1.0, "White Collar": 1.25, "Light Manual": 1.5, "Heavy Manual": 1.75 };
+        $scope.factor = ratingFactor[$scope.Premium.Occupation];           
+    };
+
+    $scope.DoCalculation = function (premium) {
+        
+        if ($scope.frmCalculate.$valid) {
+            $scope.output = (premium.SumInsured * $scope.factor * premium.Age) / (1000 * 12);
+            $("#divOutput").removeClass("hidden");
+        }
+        else {
+            alert("Please check your inputs");
+        }
+
+    };
+
+    $scope.resetForm = function () {     
+        $scope.Premium = {};
+        $("#divOutput").val();
+        $("#divOutput").addClass("hidden");
+    };
+});
