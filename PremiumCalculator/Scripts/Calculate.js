7d82aff67f676fa5828c77544219a7cf0eb6bf0c
Karthik Eetur <keetur@tcs.woolworths.com.au>
2021-06-09 15:08:37 +1000

    implemented custom directive for age

--- PremiumCalculator/Scripts/Calculate.js
+++ PremiumCalculator/Scripts/Calculate.js
@@ -1,6 +1,27 @@
-﻿
+﻿var app = angular.module('calculateApp', []);
 
-angular.module('calculateApp', []).controller('HomeController', function ($scope) {
+app.directive('numbersOnly', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attr, ngModelCtrl) {
+            function fromUser(text) {
+                if (text) {
+                    var transformedInput = text.replace(/[^0-9]/g, '');
+
+                    if (transformedInput !== text) {
+                        ngModelCtrl.$setViewValue(transformedInput);
+                        ngModelCtrl.$render();
+                    }
+                    return transformedInput;
+                }
+                return undefined;
+            }
+            ngModelCtrl.$parsers.push(fromUser);
+        }
+    };
+});
+
+app.controller('HomeController', function ($scope) {
 
     $scope.dovalidation = false;
 
